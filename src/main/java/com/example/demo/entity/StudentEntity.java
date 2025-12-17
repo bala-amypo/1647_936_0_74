53888ba52062c65968985a743b01d899f87c7644
bala-amypo <bala@amypo.in>
2025-12-17 06:26:51 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -12,4 +12,35 @@ public class StudentEntity{
     {
         return created;
     }
-    public void setCreated(date )
+    public void setCreated(date created)
+    {
+        this.created=created;
+    }
+    public Integer getId()
+    {
+        return id;
+    }
+    public void setId(Integer id)
+    {
+        this.id=id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public String getEmail()
+    {
+        return email;
+    }
+    public void setEmail(String email)
+    {
+        this.email=email;
+    }
+    public String getPasswird()
+    {
+        return password;
+    }
